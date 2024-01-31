6671209eaafd5aeaa91f1bc6f9590a0c67c36c92
NekoRouter <nekorouter@outlook.com>
2024-01-31 21:17:11 +0800

    Add swap, using PARTUUID for fstab

--- addons/opt/firstboot.sh
+++ addons/opt/firstboot.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # use all emmc free space for rootfs
-parted -s /dev/mmcblk0 "resizepart 3 -0"
-# resize root filesystem
+parted -s /dev/mmcblk0 "resizepart 4 -0"
+
+# refresh filesystem usable size
 resize2fs /dev/mmcblk0p2
-resize2fs /dev/mmcblk0p3
+resize2fs /dev/mmcblk0p4
+
+# formatand enable swap partition
+mkswap /dev/mmcblk0p3
+swapon -a
+
+# regenerate fstab
+genfstab -t PARTUUID / > /etc/fstab
 
 # regenerate openssh host keys
 dpkg-reconfigure openssh-server
+
+# reboot
+reboot
